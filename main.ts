5cd8dc0881ec966edfc274f8a8123c15742ecf5a
Tatsuto YAMAMOTO <me@laminne33569.net>
2023-11-04 22:49:56 +0900

    fix: format code

--- main.ts
+++ main.ts
@@ -1,115 +1,115 @@
 import { Hono } from "https://deno.land/x/hono@v3.8.2/mod.ts";
-import { decodeBase64, encodeBase64 } from "https://deno.land/std@0.204.0/encoding/base64.ts";
-import {v4} from "https://deno.land/std@0.204.0/uuid/mod.ts";
+import {
+  decodeBase64,
+  encodeBase64,
+} from "https://deno.land/std@0.204.0/encoding/base64.ts";
+import { v4 } from "https://deno.land/std@0.204.0/uuid/mod.ts";
 
-const app = new Hono()
+const app = new Hono();
 
-app.post("/code",async (c) => {
-    const body = await c.req.json() as {code: string}
-    const code = body.code
-    if (!code) {
-        c.status(400);
-        return c.json({error: "invalid code"});
-    }
+app.post("/code", async (c) => {
+  const body = await c.req.json() as { code: string };
+  const code = body.code;
+  if (!code) {
+    c.status(400);
+    return c.json({ error: "invalid code" });
+  }
 
-    const decoded = decodeBase64(code);
-    
-    const id = crypto.randomUUID()
-    
-    try {
-        await Deno.writeFile(`./files/input/${id}.rb`, decoded);
-    } catch {
-        c.status(500)
-        return c.json({
-            status: "failed to write file",
-            id: ""
-        })
-    }
+  const decoded = decodeBase64(code);
 
-    
+  const id = crypto.randomUUID();
+
+  try {
+    await Deno.writeFile(`./files/input/${id}.rb`, decoded);
+  } catch {
+    c.status(500);
     return c.json({
-        status: "ok",
-        id: id
-    })
-})
+      status: "failed to write file",
+      id: "",
+    });
+  }
 
-app.post("/code/:id/compile",async (c) => {
-    const id = (c.req.param() as {id: string}).id
-    
-    if (!id) {
-        c.status(400);
-        return c.json({
-            status: "failed to compile",
-            id: ""
-        })
-    }
+  return c.json({
+    status: "ok",
+    id: id,
+  });
+});
 
-    if (!v4.validate(id)) {
-        c.status(400)
-        return c.json({
-            status: "failed to compile",
-            id: ""
-        })
-    }
+app.post("/code/:id/compile", async (c) => {
+  const id = (c.req.param() as { id: string }).id;
 
-    try {
-        const a = new Deno.Command("../mrbc", {
-            args: [
-                "-o",
-                `./files/output/${id}.out`,
-                `./files/input/${id}.rb`
-            ],
-    })
-        
-        const output = await a.output();
-        const bin = await Deno.readFile(`./files/output/${id}.out`);
-        const encodedBin = encodeBase64(bin)
-        
-        return c.json({
-            binary: encodedBin,
-            error: new TextDecoder().decode(output.stderr)
-        })
+  if (!id) {
+    c.status(400);
+    return c.json({
+      status: "failed to compile",
+      id: "",
+    });
+  }
 
-    } catch (e) {
-        console.log(e)
-        c.status(500)
-        return c.json({
-            status: "failed to compile",
-            id: ""
-        })
-    }
-})
+  if (!v4.validate(id)) {
+    c.status(400);
+    return c.json({
+      status: "failed to compile",
+      id: "",
+    });
+  }
 
-app.get("/code/:id",async (c) => {
-    const id = (c.req.param() as {id: string}).id
-    
-    if (!id) {
-        c.status(400);
-        return c.json({error: "invalid id"});
-    }
+  try {
+    const a = new Deno.Command("../mrbc", {
+      args: [
+        "-o",
+        `./files/output/${id}.out`,
+        `./files/input/${id}.rb`,
+      ],
+    });
 
-    if (!v4.validate(id)) {
-        c.status(400)
-        return c.json({
-            error: "invalid id"
-        })
-    }
+    const output = await a.output();
+    const bin = await Deno.readFile(`./files/output/${id}.out`);
+    const encodedBin = encodeBase64(bin);
 
-    try {
-        const data =await Deno.readFile(`./files/input/${id}.rb`)
-        const encodedCode = encodeBase64(data)
-        
-        return c.json({
-            code: encodedCode,
-        })
+    return c.json({
+      binary: encodedBin,
+      error: new TextDecoder().decode(output.stderr),
+    });
+  } catch (e) {
+    console.log(e);
+    c.status(500);
+    return c.json({
+      status: "failed to compile",
+      id: "",
+    });
+  }
+});
 
-    } catch (e) {
-        console.log(e)
-        c.status(500)
-        return c.json({
-            error: "internal error"
-        })
-    }
-})
+app.get("/code/:id", async (c) => {
+  const id = (c.req.param() as { id: string }).id;
+
+  if (!id) {
+    c.status(400);
+    return c.json({ error: "invalid id" });
+  }
+
+  if (!v4.validate(id)) {
+    c.status(400);
+    return c.json({
+      error: "invalid id",
+    });
+  }
+
+  try {
+    const data = await Deno.readFile(`./files/input/${id}.rb`);
+    const encodedCode = encodeBase64(data);
+
+    return c.json({
+      code: encodedCode,
+    });
+  } catch (e) {
+    console.log(e);
+    c.status(500);
+    return c.json({
+      error: "internal error",
+    });
+  }
+});
 
 Deno.serve(app.fetch);
--- readme.md
+++ readme.md
@@ -1,53 +1,61 @@
-# kanicc - *Compile Server for Matz葉ガニロボコン*
+# kanicc - _Compile Server for Matz葉ガニロボコン_
 
 ## API Documents
 
 ### POST /code
+
 コードをアップロード
 
 #### Request
+
 ```json
 {
-    "code": "<base64 encoded Ruby Code>"
+  "code": "<base64 encoded Ruby Code>"
 }
 ```
 
 #### Response
+
 `Status 200`
+
 ```json
 {
-    "status": "ok",
-    "id": "387972832krgr-3jekfjn4t-rkjgnjnedwdgr"
+  "status": "ok",
+  "id": "387972832krgr-3jekfjn4t-rkjgnjnedwdgr"
 }
 ```
 
 ### GET /code/:id
+
 コードが存在するかのチェック
 
 #### Response
+
 `Status 200`
+
 ```json
 {
-    "code": "<base64 encoded Ruby Code>"
+  "code": "<base64 encoded Ruby Code>"
 }
 ```
 
-
 ### POST /code/:id/compile
+
 コードをコンパイル
 
 #### Request
+
 ```json
 {}
 ```
 
 #### Response
+
 `200 OK`
 
 ```json
 {
-    "binary": "<base64 encoded mruby/c binary>",
-    "error": "<compiler output>"
+  "binary": "<base64 encoded mruby/c binary>",
+  "error": "<compiler output>"
 }
 ```
-
